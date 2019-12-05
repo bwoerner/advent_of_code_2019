7bc6d13ebdd3674371220aa32b77e878f41d9283
bwoerner <woerneea@gmail.com>
2019-12-04 21:50:06 -0500

    add comments

--- day4/day4.py
+++ day4/day4.py
@@ -4,29 +4,35 @@ stop = 843212
 
 #part one 
 def is_qualified(num):
+
     x = num
     num = str(num)
-    if len(set(num)) == len(num):
+
+    if sorted(num) != list(num): #determine if the list is in order 
         return False
-    elif sorted(num) != list(num):
+    elif len(set(num)) == len(num): #determine if there are duplicate numbers
         return False
+    
     return True
 
 
 
 #part two
 def is_qualified_num(num):
+
     num = str(num)
-    if sorted(num) != list(num):
+
+    if sorted(num) != list(num): #determine if the list is in order
         return False
     for i in num:
-        if num.count(i) == 2:
+        if num.count(i) == 2: #determine if the list has exactly two duplicate numbers
             return True
     return False
 
 
 total = 0
 end = stop + 1
+
 for i in range(start, end):
     if is_qualified_num(i):
         total += 1
